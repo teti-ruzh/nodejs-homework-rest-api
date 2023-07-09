7d256154a0968adb1d07b23860b8319e71313613
Tetiana Ruzhetska <tetiana.ruzhetska@gmail.com>
2023-07-09 20:17:36 +0300

    add registration and login

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://Tetiana:NCv6ZpjO7fHuJCqM@cluster0.bqj49hr.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST } = process.env;
 
 mongoose.set("strictQuery", true);
 
